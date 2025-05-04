0e2cd4f29a9429f375cb71dc568f811e06462844
ShotgunApe <shotgunayyype@gmail.com>
2025-05-04 17:03:30 -0400

    Finished torch version

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import tensorflow as tf
 import torch as torch
 import numpy as np
 
+import warnings
+warnings.filterwarnings("ignore")
+
 #tf.debugging.set_log_device_placement(True)
 #tf.config.list_physical_devices(device_type = None)
 
@@ -26,11 +29,13 @@ def gj(A,b):
                         A[k,j],A[i,j] = A[i,j],A[k,j]
                     b[k],b[i] = b[i],b[k]
                     break
+        print(f"b after swap {k}: {b}")
         # Division
         pivot = A[k,k]
         for j in range(k,n): 
             A[k,j] /= pivot
         b[k] /= pivot
+        print(f"b after division {k}: {b}")
         #Elimination
         for i in range(n): 
             if i == k or A[i,k] == 0: 
@@ -39,56 +44,66 @@ def gj(A,b):
             for j in range(k,n): 
                 A[i,j] -= factor*A[k,j]
             b[i] -= factor*b[k]
+
+        print(f"b after elimination {k}: {b}")
     
     return b, A
 
-def g_j_torch(A, b):
-    A = torch.tensor(A)
-    b = torch.tensor(b)
-    for k in range(n):
-        if torch.abs(A[k,k]) < 1.0e-12: 
-            for i in range(k+1, n): 
-                if torch.abs(A[i,k]) > torch.abs(A[k,k]): 
-                    temp = A[k].clone()
-                    A[k] = A[i]
-                    A[i] = temp
-                    temp = b[k].clone()
-                    b[k] = b[i]
-                    b[i] = temp
-                    break
+def gj_torch(A, b):
+    A = torch.tensor(A, dtype=torch.float)
+    b = torch.tensor(b, dtype=torch.float)
+    n = len(A)
 
-def row_to_top_torch(A, b, k, n):
-    if torch.abs(A[k,k]) < 1.0e-12: 
+    b = torch.reshape(b, (n, 1))
+    Ab = torch.cat((A, b), dim = 1)
+
+    for k in range(n):
+        row_to_top_torch(Ab, k, n)
+
+        # Division
+        pivot = Ab[k, k].clone()
+        Ab[k] = torch.div(Ab[k], pivot)
+
+        # Elimination
+        for i in range(n):
+            if i == k or Ab[i,k] == 0:
+                continue
+            factor = Ab[i,k].clone()
+            Ab[i, k:] = torch.sub(Ab[i, k:], torch.mul(Ab[k, k:], factor))
+
+    return Ab[:, n:], Ab[:, :n]
+
+
+
+def row_to_top_torch(Ab, k, n):
+    if torch.abs(Ab[k,k]) < 1.0e-12: 
         for i in range(k+1, n): 
-            if torch.abs(A[i,k]) > torch.abs(A[k,k]): 
-                temp = A[k].clone()
-                A[k] = A[i]
-                A[i] = temp
-                temp = b[k].clone()
-                b[k] = b[i]
-                b[i] = temp
+            if torch.abs(Ab[i,k]) > torch.abs(Ab[k,k]): 
+                temp = Ab[k].clone()
+                Ab[k] = Ab[i]
+                Ab[i] = temp
                 break
 
-A = torch.tensor([
+A = [
     [0,2,0,1], 
     [2,2,3,2], 
     [4,-3,0,1], 
     [6,1,-6,-5]
-    ])
-b = torch.tensor([0,-2,-7,6])
-
-print(A, b)
-row_to_top_torch(A, b, 0, 4)
-print(A, b)
-
-A = np.array([
-    [0,2,0,1], 
-    [2,2,3,2], 
-    [4,-3,0,1], 
-    [6,1,-6,-5]
-    ])
-b = np.array([0,-2,-7,6])
+    ]
+b = [0,-2,-7,6]
 
 x,a = gj(A,b)
+
+print(f"Solution: {x}")
+
+A = [
+    [0,2,0,1], 
+    [2,2,3,2], 
+    [4,-3,0,1], 
+    [6,1,-6,-5]
+    ]
+b = [0,-2,-7,6]
+
+x,a = gj_torch(A,b)
 
 print(f"Solution: {x}")
